9a34dea7d94e0136c8d34ab7fa852301d9234397
Vojtěch Kolenčík <vojtech.kolencik@e-invent.eu>
2017-11-14 12:46:26 +0100

    event handling

--- index.js
+++ index.js
@@ -26,10 +26,13 @@ app.post('/webhook', (req, res) => {
         // Iterates over each entry - there may be multiple if batched
         body.entry.forEach(function(entry) {
 
-            // Gets the message. entry.messaging is an array, but
-            // will only ever contain one message, so we get index 0
-            let webhookEvent = entry.messaging[0];
-            console.log(webhookEvent);
+            // Gets the body of the webhook event
+            let webhook_event = entry.messaging[0];
+            console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log('Sender PSID: ' + sender_psid);
         });
 
         // Returns a '200 OK' response to all requests
@@ -70,3 +73,18 @@ app.get('/webhook', (req, res) => {
         }
     }
 });
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+
+}
